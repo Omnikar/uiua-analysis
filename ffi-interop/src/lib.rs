752cb887f50cea5bcc11f0437c39a7a406edb37e
Omnikar <omnikar5@gmail.com>
2026-01-14 16:27:07 -0500

    Create cargo workspace and `ffi-interop` subcrate

---
+++ ffi-interop/src/lib.rs
@@ -0,0 +1,43 @@
+use std::{ffi, slice};
+
+pub struct ArrayRef<'a, T> {
+    pub rank: usize,
+    pub shape: &'a [usize],
+    pub strides: &'a [usize],
+    pub data: &'a [T],
+}
+
+#[repr(C)]
+pub struct FfiUnrankedMemRef {
+    rank: usize,
+    descriptor: *const ffi::c_void,
+}
+
+#[repr(C)]
+struct FfiMemRefDescriptorHeader<T> {
+    allocated: *const T,
+    aligned: *const T,
+    offset: usize,
+    rest: ffi::c_void,
+}
+
+pub fn extract_memref<'a, T>(memref: FfiUnrankedMemRef) -> ArrayRef<'a, T> {
+    let descr_ptr = memref.descriptor as *const FfiMemRefDescriptorHeader<T>;
+    let descr = unsafe { &*descr_ptr };
+
+    let shape_ptr = &descr.rest as *const ffi::c_void as *const usize;
+    let shape = unsafe { slice::from_raw_parts(shape_ptr, memref.rank) };
+
+    let strides_ptr = unsafe { shape_ptr.add(memref.rank) };
+    let strides = unsafe { slice::from_raw_parts(strides_ptr, memref.rank) };
+
+    let elem_count: usize = shape.iter().copied().product();
+    let data = unsafe { slice::from_raw_parts(descr.aligned, elem_count) };
+
+    ArrayRef {
+        rank: memref.rank,
+        shape,
+        strides,
+        data,
+    }
+}
