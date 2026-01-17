9aa1f44b434f06e8e4c38108080e5490420c7254
Omnikar <omnikar5@gmail.com>
2026-01-16 22:43:15 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -12,18 +12,18 @@ name=test
 
 LLVM_DIR=$(llvm-config --libdir)
 
-mlir-opt $name.mlir --pass-pipeline='builtin.module(func.func(tosa-to-linalg),tosa-to-arith,convert-elementwise-to-linalg,one-shot-bufferize{bufferize-function-boundaries},func.func(buffer-hoisting,buffer-loop-hoisting),drop-equivalent-buffer-results,func.func(promote-buffers-to-stack),buffer-deallocation-pipeline,convert-bufferization-to-memref,expand-strided-metadata,convert-linalg-to-affine-loops,lower-affine,finalize-memref-to-llvm,convert-scf-to-cf,convert-to-llvm,reconcile-unrealized-casts)' -o $name_opt.mlir
+mlir-opt "$name".mlir --pass-pipeline='builtin.module(symbol-dce,cse,func.func(tosa-to-linalg),tosa-to-arith,convert-elementwise-to-linalg,one-shot-bufferize{bufferize-function-boundaries},func.func(buffer-hoisting,buffer-loop-hoisting),drop-equivalent-buffer-results,func.func(promote-buffers-to-stack),buffer-deallocation-pipeline,convert-bufferization-to-memref,expand-strided-metadata,convert-linalg-to-affine-loops,lower-affine,finalize-memref-to-llvm,convert-scf-to-cf,convert-to-llvm,reconcile-unrealized-casts,canonicalize)' -o "$name"_opt.mlir
 
-mlir-translate $name_opt.mlir \
+mlir-translate "$name"_opt.mlir \
   -mlir-to-llvmir \
-  -o $name_opt.ll
+  -o "$name"_opt.ll
 
-llc -filetype=obj --relocation-model=pic $name_opt.ll -o $name.o
+llc -filetype=obj --relocation-model=pic "$name"_opt.ll -o "$name".o
 
-clang $name.o \
+clang "$name".o \
   -L $LLVM_DIR \
   -lmlir_c_runner_utils \
   -L ../target/release \
   -L ../target/debug \
   -lstdlib \
-  -o $name
+  -o "$name"
